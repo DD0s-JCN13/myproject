911cfb21470814c20827f6719cc06716fc66343d
DD0s-JCN13 <jimmylin160@gmail.com>
2017-12-04 08:26:26 +0800

    bublesoft num edit

--- src/com/garmin/BubleSoft.java
+++ src/com/garmin/BubleSoft.java
@@ -4,7 +4,7 @@ public class BubleSoft {
 	public static void main (String[] args){
 		int tmp = 0;
 		//int n[] = {51, 23, 6, 65, 15};
-		int n[] = {7, 13, 26, 53, 36, 28, 47};
+		int n[] = {13, 7, 26, 53, 36, 28, 47, 49, 37};
 		for(int i=0;i<n.length-1;i++){
 			for(int j=i+1;j<n.length;j++){
 				if(n[i]>n[j]){
