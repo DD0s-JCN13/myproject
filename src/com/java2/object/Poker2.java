9a00d4f27c454028270a548fad3d45bf589078f2
DD0s-JCN13 <jimmylin160@gmail.com>
2018-03-30 19:38:47 +0800

    poker2 Fin

--- src/com/java2/object/Poker2.java
+++ src/com/java2/object/Poker2.java
@@ -6,15 +6,39 @@ public class Poker2 {
 	Random random = new Random();
 	public static ArrayList<String> cards = new ArrayList<>(); 
 	public static String flowers = "SHDC";
-	public  void poker(){
+	public Poker2(){
 		for (int i = 0;i< flowers.length();i++){
 			for (int j = 1;j <= 13; j++){
-				cards.add(flowers.charAt(j)+"i");
+				String tmpi = String.valueOf(j);
+				cards.add(i,flowers.charAt(i)+tmpi);
+				if(j%13!=0) {
+					System.out.print(cards.get(i)+" ");
+				}
+				else {
+					System.out.println(cards.get(i));
+				}
 			}
 		}
 	}
-	public  static ArrayList<String> ReturnPoker() {
-		return cards;
+	
+	public void Suffle() {
+		for (int i = 0; i < cards.size(); i++) {
+			int ram = random.nextInt(53-1);
+			String tmp = cards.get(i);
+			String tmp2 = cards.get(ram);
+			cards.set(ram, tmp);
+			cards.set(i, tmp2);
+			
+		}
+	}
+	public void Show() {
+		for (int i = 0; i< cards.size(); i++) {
+			if ((i+1) % 13 == 0) {
+				System.out.println(cards.get(i));
+			}else {
+				System.out.print(cards.get(i)+" ");
+			}
+		}
 	}
 	
 	
--- src/com/java2/object/PokerTester.java
+++ src/com/java2/object/PokerTester.java
@@ -1,14 +1,12 @@
 package com.java2.object;
-import java.util.ArrayList;
 public class PokerTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String flowers = Poker2.flowers;
-		ArrayList<String> cards = Poker2.ReturnPoker() ;
-		for (int i = 0; i<(flowers.length())*13;i++) {
-			System.out.println(cards.get(i));
-		}
+		Poker2 poker= new Poker2();
+		System.out.println("");
+		poker.Suffle();
+		poker.Show();
 	}
 
 }
