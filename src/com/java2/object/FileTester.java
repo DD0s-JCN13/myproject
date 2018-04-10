2f9ab79e68255fe551b751bb73b7134b4dd9f5bc
DD0s_JCN13 <jimmylin160@gmail.com>
2018-04-10 16:26:25 +0800

    Build FileTester

---
+++ src/com/java2/object/FileTester.java
@@ -0,0 +1,22 @@
+package com.java2.object;
+
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+public class FileTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+			FileOutputStream out = new FileOutputStream("data.txt");
+			out.write(75);
+			out.flush();
+			out.close();
+		} catch (FileNotFoundException e){
+			System.out.println("Flie NOT FOUND!!");
+		}catch (IOException e){
+			System.out.println("File Output problem");
+		}
+	}
+
+}
