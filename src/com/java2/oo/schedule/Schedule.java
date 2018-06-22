6598f07360cf32a69647f77c414d2613084629d4
DD0s-JCN13 <jimmylin160@gmail.com>
2018-06-22 17:48:04 +0800

    scheduld_2 tmp

--- src/com/java2/oo/schedule/Schedule.java
+++ src/com/java2/oo/schedule/Schedule.java
@@ -1,11 +1,41 @@
 package com.java2.oo.schedule;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Schedule {
 	public static void main(String args[]) {
 		Main m = new Main();
 		FileInputStream fis;
+		try {
+			fis = new FileInputStream("Schedule.txt");
+			InputStreamReader isr = new InputStreamReader(fis);
+			BufferedReader in = new BufferedReader(isr);
+			String line = in.readLine();
+			while(line!=null) {
+				String[] token = line.split(",");
+				try {
+					String room = token[0];
+					String course = token[1];
+					int day = Integer.parseInt(token[2]);
+					int time = Integer.parseInt(token[3]);
+					int dur = Integer.parseInt(token[4]);
+					m.courses.add(new Course(room,course,day,time,dur));
+				} catch (NumberFormatException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				line = in.readLine();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		
 		
