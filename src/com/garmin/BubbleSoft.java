d3f3287f46bf1f6ade5329000ebdb45e97903c14
DD0s-JCN13 <jimmylin160@gmail.com>
2018-09-29 14:37:00 +0800

    BubbleSoft editted

--- src/com/garmin/BubbleSoft.java
+++ src/com/garmin/BubbleSoft.java
@@ -1,10 +1,10 @@
 package com.garmin;
 
-public class BubleSoft {
+public class BubbleSoft {
 	public static void main (String[] args){
 		int tmp = 0;
 		//int n[] = {51, 23, 6, 65, 15};
-		int n[] = {13, 7, 26, 53, 36, 28, 47, 49, 37};
+		int n[] = {13, 7, 53, 26, 36, 28, 47, 49, 37};
 		for(int i=0;i<n.length-1;i++){
 			for(int j=i+1;j<n.length;j++){
 				if(n[i]>n[j]){
@@ -13,6 +13,11 @@ public class BubleSoft {
 					n[j]=tmp;
 				}
 			}
+			System.out.print("第"+(i+1)+"次執行結果：");
+			for(int k=0;k<n.length;k++) {
+				System.out.print(n[k]+" ");
+			}
+			System.out.println("");
 		}
 		for(int k=0;k<n.length;k++){
 		System.out.print(n[k]+" ");
